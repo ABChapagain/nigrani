59d668b4ef8d04f523cdfa3303fc9fd6667a5fdb
Rejens <rejensraya@gmail.com>
2024-02-16 01:21:31 +0545

    Commented out unused code in Portals.jsx

--- frontend/src/components/Portals.jsx
+++ frontend/src/components/Portals.jsx
@@ -1,9 +1,9 @@
 const Portals = () => {
   return (
-    <>
-      <div id='popper-portal'></div>
-      <div id='modal-portal'></div>
-    </>
+   //  <>
+   //    <div id='popper-portal'></div>
+   //    <div id='modal-portal'></div>
+   //  </>
   )
 }
 
