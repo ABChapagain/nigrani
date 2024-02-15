f1a4d03f33f056d4d2553298193008ee2123e909
Rejens <rejensraya@gmail.com>
2024-02-16 01:42:22 +0545

    Update close button styling in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,9 +62,9 @@ function App({ children }) {
                 onClick={() => setShowModal(false)}
                 className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               >
-                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                {/* <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
+                </svg> */}
                 <span className="sr-only">Close modal</span>
               </button>
             </div>
