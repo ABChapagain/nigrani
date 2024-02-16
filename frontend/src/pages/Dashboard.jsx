df3041c0cdd03f110e2cab23c4fab086e875933b
Prashant <meprazhant@gmail.com>
2024-02-16 22:52:25 +0545

    Update Dashboard heading

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>DashBoard</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
